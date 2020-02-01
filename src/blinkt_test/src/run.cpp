4a3840c13bdbaa7862296ed0f0418a2b6da7ab51
James Pace <jpace121@gmail.com>
2020-02-01 21:36:51 +0000

    Add run blink test.

---
+++ src/blinkt_test/src/run.cpp
@@ -0,0 +1,34 @@
+// Copyright 2020 James Pace
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#include "blinkt_interface/blinkt.hpp"
+#include "blinkt_interface/color.hpp"
+#include <chrono>
+#include <thread>
+
+using namespace std::chrono_literals;
+
+int main(int argc, char *argv[])
+{
+    blinkt_interface::Blinkt blinkt{};
+
+    for(int i = 0; i < blinkt.number_of_pixels(); i++)
+    {
+        blinkt.setPixel(i, blinkt_interface::color::red(0.5));
+        blinkt.display();
+        std::this_thread::sleep_for(1s);
+        blinkt.setPixel(i, blinkt_interface::color::off(0.5));
+        blinkt.display();
+    }
+    return 0;
+}
