cae7f27e88ce209babe72781bcc11cde7ba8cf87
James Pace <jpace121@gmail.com>
2020-02-03 01:23:59 +0000

    Realistic pub launch file.

--- src/j7s/launch/j7s-pub.launch.py
+++ src/j7s/launch/j7s-pub.launch.py
@@ -9,7 +9,7 @@ def generate_launch_description():
             node_name='aqua_pub',
             output='log',
             parameters=[{
-                'freq': 1.0,
+                'freq': 0.5,
                 'led_index': 0,
                 'color': 'aqua',
                 'pub_freq': 10.0,
@@ -25,9 +25,41 @@ def generate_launch_description():
             node_name='red_pub',
             output='log',
             parameters=[{
+                'freq': 1.0,
+                'led_index': 1,
+                'color': 'red',
+                'pub_freq': 10.0,
+                'brightness': 0.5
+            }],
+            remappings = [
+                ('led_state', 'j7s_led_state')
+            ]
+        ),
+        launch_ros.actions.Node(
+            package='j7s',
+            node_executable='j7s-pub',
+            node_name='white_pub',
+            output='log',
+            parameters=[{
+                'freq': 2.0,
+                'led_index': 2,
+                'color': 'white',
+                'pub_freq': 10.0,
+                'brightness': 0.5
+            }],
+            remappings = [
+                ('led_state', 'j7s_led_state')
+            ]
+        ),
+        launch_ros.actions.Node(
+            package='j7s',
+            node_executable='j7s-pub',
+            node_name='green_pub',
+            output='log',
+            parameters=[{
                 'freq': 2.0,
                 'led_index': 3,
-                'color': 'red',
+                'color': 'green',
                 'pub_freq': 10.0,
                 'brightness': 0.5
             }],
